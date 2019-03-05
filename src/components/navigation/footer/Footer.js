7c54cbdecbb662f3329524a39978adf2cb5eb679
manish <manish91mgr@gmail.com>
2019-03-05 23:49:40 +0530

    second

--- src/components/navigation/footer/Footer.js
+++ src/components/navigation/footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 const footer = (props) => {
     return (
         <div className='foot'>
-            <span className='foot-text' >Copyright &copy; 2019 HighRadius Technology Pvt Ltd. All rights reserved.</span>
+            <span className='foot-text' >Copyright &copy; 2019 All rights reserved.</span>
         </div>
     );
 };
